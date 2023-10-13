035c9c43d657852ce693aca086ac11f40449c009
donotprikel231 <118140095+donotprikel231@users.noreply.github.com>
2023-10-12 21:20:30 -0700

    No

--- JS/hack1.js
+++ JS/hack1.js
@@ -1,19 +1,16 @@
 (function() {
     'use strict';
 
-    // Delete the element with class "preview-slot"
     var previewSlot = document.querySelector('.ytp-ad-preview-slot');
     if (previewSlot) {
         previewSlot.remove();
     }
 
-    // Change the display property of elements with class "button-slot" to "flex"
     var buttonSlots = document.querySelectorAll('.ytp-ad-skip-button-slot');
     buttonSlots.forEach(function(buttonSlot) {
         buttonSlot.style.display = 'flex';
     });
 
-    // Change the display property of elements with class "button-container" to "flex"
     var buttonContainers = document.querySelectorAll('.ytp-ad-skip-button-container');
     buttonContainers.forEach(function(buttonContainer) {
         buttonContainer.style.display = 'flex';
@@ -28,9 +25,7 @@
         }
     }
 
-    // Replace 'your-button-class' with the actual class name of the button you want to click
     const targetClassName = 'ytp-ad-skip-button.ytp-button';
 
-    // Execute the click function
     clickButtonByClassName(targetClassName);
 })();
