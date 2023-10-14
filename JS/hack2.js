c6c944a642c73614dbc8ccceb55e18c229b6a5f9
donotprikel231 <118140095+donotprikel231@users.noreply.github.com>
2023-10-14 09:24:33 -0700

    ABOSUTLY NO

--- JS/hack2.js
+++ JS/hack2.js
@@ -1,10 +1,25 @@
 (function() {
     'use strict';
 
-    function skipToEndOfVideo() {
+    function adDec() {
+        console.log("15 Second ad detected");
+    }
+
+    function detectElement() {
+        const element1 = document.getElementById("ad-text:2");
+        const element2 = document.getElementById("ad-text:4");
+        if (element1 || element2) {
+            skipToEndOfAd()
+            adDec();
+        }
+    }
+
+    function skipToEndOfAd() {
         const videoElement = document.querySelector('video');
         if (videoElement) {
             videoElement.currentTime = videoElement.duration;
         }
     }
+
+    detectElement();
 })();
