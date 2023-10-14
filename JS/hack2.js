0e5cc520244a6f741d0432897bce5cda60aa3873
donotprikel231 <118140095+donotprikel231@users.noreply.github.com>
2023-10-14 09:28:48 -0700

    HECK NO

--- JS/hack2.js
+++ JS/hack2.js
@@ -6,9 +6,10 @@
     }
 
     function detectElement() {
-        const element1 = document.getElementById("ad-text:2");
-        const element2 = document.getElementById("ad-text:4");
-        if (element1 || element2) {
+        const element2 = document.getElementById("ad-text:2");
+        const element3 = document.getElementById("ad-text:3");
+        const element4 = document.getElementById("ad-text:4");
+        if (element2 || element3 || element4) {
             skipToEndOfAd()
             adDec();
         }
